b80644814fa9631a011d16e1b1ac58b114851622
Jake Kovaleski <61708246+jakekova@users.noreply.github.com>
2023-04-16 13:57:52 -0400

    for loop start

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,9 +8,13 @@ next(csv_file)
 
 
 months = 0
-net_total = 0.0
-greatest_inc = 0.0
-greatest_dec = 0.0
+net_total = 0
+greatest_inc = 0
+greatest_dec = 0
+avg_list = []
 
-
+for row in csv_reader:
+    months += 1
+    net_total += int(row[1])
+    avg_list.append = int(row[1])
 
