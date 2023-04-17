f0505fd7cca44838a5508ff845bd052bf11270ee
Jake Kovaleski <61708246+jakekova@users.noreply.github.com>
2023-04-16 22:47:52 -0400

    pybank done without text export

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,6 @@
 import csv
 import statistics
+import sys
 
 path = "Resources/budget_data.csv"
 
@@ -14,19 +15,24 @@ net_total = 0
 greatest_inc = 0
 greatest_dec = 0
 avg_list = []
-
+b = 0
 
 
 for row in csv_reader:
     months += 1
     net_total += int(row[1])
-    avg_list.append(int(row[1]))
-    if int(row[1]) > greatest_inc:
-        greatest_inc = int(row[1])
+    a = int(row[1])
+    avg_list.append(int(a - b))
+    if int(a - b) > greatest_inc:
+        greatest_inc = int(a - b)
         greatest_inc_date = row[0]
-    elif int(row[1]) < greatest_dec:
-        greatest_dec = int(row[1])
+    elif int(a - b) < greatest_dec:
+        greatest_dec = int(a - b)
         greatest_dec_date = row[0]
+
+    b = int(row[1])
+
+
 
 print("Financial Analysis")
 print('')
@@ -38,6 +44,7 @@ print(f'Total: ${net_total}')
 print('')
 print(f'Average Change: ${round(statistics.fmean(avg_list), 2)}')
 print('')
-print(f'Greatest Increase in Profits: {greatest_inc_date} : {greatest_inc}')
+print(f'Greatest Increase in Profits: {greatest_inc_date} : ${greatest_inc}')
 print('')
-print(f'Greatest Decrease in Profits: {greatest_dec_date} : {greatest_dec}')
+print(f'Greatest Decrease in Profits: {greatest_dec_date} : ${greatest_dec}')
+print(avg_list)
