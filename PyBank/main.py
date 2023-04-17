157937a0c3a0a36c20df28e6e927f38fbc43a2ca
Jake Kovaleski <61708246+jakekova@users.noreply.github.com>
2023-04-16 20:45:26 -0400

    runs good, wrong task

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,6 @@
 import csv
+import statistics
+
 path = "Resources/budget_data.csv"
 
 csv_file = open(path)
@@ -13,8 +15,29 @@ greatest_inc = 0
 greatest_dec = 0
 avg_list = []
 
+
+
 for row in csv_reader:
     months += 1
     net_total += int(row[1])
-    avg_list.append = int(row[1])
+    avg_list.append(int(row[1]))
+    if int(row[1]) > greatest_inc:
+        greatest_inc = int(row[1])
+        greatest_inc_date = row[0]
+    elif int(row[1]) < greatest_dec:
+        greatest_dec = int(row[1])
+        greatest_dec_date = row[0]
 
+print("Financial Analysis")
+print('')
+print("---------------------")
+print('')
+print(f"Total Months: {months}")
+print('')
+print(f'Total: ${net_total}')
+print('')
+print(f'Average Change: ${round(statistics.fmean(avg_list), 2)}')
+print('')
+print(f'Greatest Increase in Profits: {greatest_inc_date} : {greatest_inc}')
+print('')
+print(f'Greatest Decrease in Profits: {greatest_dec_date} : {greatest_dec}')
